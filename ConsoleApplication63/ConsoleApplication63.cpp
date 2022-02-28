33e118d7ad6d64f704802dd5485b1630f4b44dd7
dmitr <dmitr@LAPTOP-UCOCAVOT>
2022-02-28 13:36:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication63/ConsoleApplication63.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+const int ARR_LEN = 6;
+
+void main()
+{
+    setlocale(LC_CTYPE, "Russian");
+
+    float arr[ARR_LEN];
+    float sum = 0;    
+
+    for (int i = 0; i < ARR_LEN; i++) {
+        cout << "Введите элемент массива " << i << ": ";
+        cin >> arr[i]; 
+        sum += arr[i]; 
+    }
+
+    cout << "Среднее арифметическое чисел в массиве: " << sum / ARR_LEN << endl;
+
+    system("pause");
+}
